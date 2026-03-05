c8a70e31778a0e449915fcedf13da2416f8b3cd0
faizanraza8009553488-code <faizanraza8009553488@mpgi.edu.in>
2026-03-05 14:45:00 +0530

    Create wireshark.py

---
+++ wireshark.py
@@ -0,0 +1,26 @@
+# Imports
+from scapy.all import sniff, IP
+import tkinter as tk
+from tkinter import ttk
+import threading
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import time
+
+# Global variables
+sniffing = False
+packet_count = 0
+
+# Functions
+def process_packet(packet):
+    pass
+
+def start_sniff():
+    pass
+
+def stop_sniff():
+    pass
+
+# GUI
+root = tk.Tk()
+root.mainloop()
